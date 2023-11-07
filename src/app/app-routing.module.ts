84623344593117d160ce1802b61bc80796a7e5a0
Harichandana <harichandanavita@gmail.com>
2023-11-07 11:37:35 +0530

    router path  created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "header",
+    component: HeaderComponent
+  },
+  {
+    path: "section",
+    component: SectionComponent
+  },
+  {
+    path: "footer",
+    component: FooterComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
