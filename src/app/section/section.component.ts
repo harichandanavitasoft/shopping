4dfd1ff612a7a2fa06e34a2d993b71526db6a61d
Harichandana <harichandanavita@gmail.com>
2023-11-07 12:20:10 +0530

    print apidata using ng for

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -7,12 +7,15 @@ import { ApiService } from '../api.service';
   styleUrls: ['./section.component.css']
 })
 export class SectionComponent implements OnInit {
+  apiData!: any;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.apiService.getData().subscribe((res: any) => {
       console.log(res);
+      this.apiData = res
+      console.log(this.apiData);
     });
   }
 }
